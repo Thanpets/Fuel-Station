3075d60aa6be300a87d2bfecb08cad0c8c75305e
Alexis Papazoglou <alpapazog@gmail.com>
2021-06-26 13:06:44 +0300

    Form1 changes

--- FuelStationProgram/FuelStationProgram/Form1.Designer.cs
+++ FuelStationProgram/FuelStationProgram/Form1.Designer.cs
@@ -29,13 +29,96 @@ namespace FuelStationProgram
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtSqlPath = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.btnConnect = new DevExpress.XtraEditors.SimpleButton();
+            this.btnLoadData = new DevExpress.XtraEditors.SimpleButton();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSqlPath.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSqlPath
+            // 
+            this.txtSqlPath.EditValue = resources.GetString("txtSqlPath.EditValue");
+            this.txtSqlPath.Location = new System.Drawing.Point(62, 20);
+            this.txtSqlPath.Name = "txtSqlPath";
+            this.txtSqlPath.Size = new System.Drawing.Size(390, 20);
+            this.txtSqlPath.TabIndex = 0;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(12, 23);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(44, 13);
+            this.labelControl1.TabIndex = 1;
+            this.labelControl1.Text = "SQL Path";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(458, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(98, 36);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(562, 12);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(98, 36);
+            this.btnLoadData.TabIndex = 4;
+            this.btnLoadData.Text = "Load Data";
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click);
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Location = new System.Drawing.Point(12, 87);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(776, 277);
+            this.gridControl1.TabIndex = 5;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.btnLoadData);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.txtSqlPath);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtSqlPath.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private DevExpress.XtraEditors.TextEdit txtSqlPath;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.SimpleButton btnConnect;
+        private DevExpress.XtraEditors.SimpleButton btnLoadData;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
     }
 }
 
--- FuelStationProgram/FuelStationProgram/Form1.cs
+++ FuelStationProgram/FuelStationProgram/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,57 @@ using System.Windows.Forms;
 
 namespace FuelStationProgram
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+    public partial class Form1 : Form {
+        public string ConnString { get; set; }
+        public SqlConnection Conn { get; set; }
+        public DataSet MasterData = new DataSet();
+        public Form1() {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e) {
+            
+            SqlConnect();
+        }
+
+        private void SqlConnect() {
+            ConnString = txtSqlPath.EditValue.ToString();
+            try {
+                Conn = new SqlConnection(ConnString);
+                Conn.Open();
+                //Console.WriteLine(conn.State);
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnLoadData_Click(object sender, EventArgs e) {
+            SqlLoadTables();
+        }
+
+        private void SqlLoadTables() {
+            try {
+
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM [dbo].[Customers]", Conn);
+                adapter.Fill(MasterData);
+                gridControl1.DataSource = MasterData.Tables[0];
+
+                gridControl1.Refresh();
+
+
+                gridView1.OptionsView.ShowGroupPanel = false;
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+            
         }
     }
 }
